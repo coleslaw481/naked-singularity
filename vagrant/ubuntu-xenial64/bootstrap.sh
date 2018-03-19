946bb4ccd361f33b1b02f00465cdf3702f6fffc5
Chris Churas <churas.camera@gmail.com>
2018-03-19 15:57:03 -0700

    initial revision

---
+++ vagrant/ubuntu-xenial64/bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo "Installing base packages"
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install build-essential python debootstrap
+
+wget https://github.com/singularityware/singularity/releases/download/2.3.2/singularity-2.3.2.tar.gz
+tar xvf singularity-2.3.2.tar.gz
+cd singularity-2.3.2
+./configure --prefix=/usr/local
+make
+make install
+
